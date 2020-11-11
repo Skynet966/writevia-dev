1529cff2e3fce20d14611f9aa62d6bd6c0d29444
Skynet966 <satishkumaryadav72@gmail.com>
2020-11-11 07:13:48 +0530

    weather location fetching update

--- components/widgets/weather/Weather.js
+++ components/widgets/weather/Weather.js
@@ -6,9 +6,10 @@ export default class Weather extends React.Component{
         this.options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
         this.state ={city:null,country:null,temp:null,desc:null,icon:null,lat:null,lon:null};
     }
-    getWeather(){
-        if(this.state.lat&this.state.lon){
-            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}&units=metric&appid=7d0aef1d6da83a6ba6a6cb629993f2f3`;
+    getWeather(lat,lon){
+        if(lat&&lon){
+            console.log(lat,lon)
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=7d0aef1d6da83a6ba6a6cb629993f2f3`;
             const promise = fetch(url);
             promise.then(response=>{
                 response.json().then(weather=>{
@@ -23,16 +24,12 @@ export default class Weather extends React.Component{
         }
     }
     getLocation(){
-        const url = `http://api.ipstack.com/check?access_key=1b898153ec3106e7861abe88bb76ee61&format=1`;
-        const promise = fetch(url);
-        promise.then(response=>{
-            response.json().then(location=>{
-                this.setState({lat:location.latitude,lon:location.longitude});
-                this.getWeather();
-            })
-        }).catch(err=>{
-            console.log("There is some error in iplocate");
-        })
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(pos=>this.getWeather(pos.coords.latitude,pos.coords.longitude));
+        } else {
+            console.log("Not Available");
+        }
+        // const url = `http://api.ipstack.com/check?access_key=1b898153ec3106e7861abe88bb76ee61&format=1`;
     }
     componentDidMount(){
         this.getLocation();
